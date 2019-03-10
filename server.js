fd1cbbd6190e9773a079b7e615d1bf4c09328827
georgeC0stanza <32494929+georgeC0stanza@users.noreply.github.com>
2019-03-09 20:48:01 -0700

    mail time test!

--- server.js
+++ server.js
@@ -1,59 +1,21 @@
 
 var express = require("express");
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
 var app = express();
-
 app.use(express.static("public"));
 
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {
-	console.log("Received a request for /");
-
-	res.write("This is the root.");
-	res.end();
-});
-
-app.get("/home", function(req, res) {
-	// Controller
-	console.log("Received a request for the home page");
-	var name = getCurrentLoggedInUserAccount();
-	var emailAddress = "john@email.com";
-
-	var params = {username: name, email: emailAddress};
-
-	res.render("home", params);
-});
-
-app.listen(process.env.PORT, function() {
+app.listen(PORT, function() {
 	console.log("The server is up and listening on port 5000");
 });
 
-// Model
-
-function getCurrentLoggedInUserAccount() {
-	// access the database
-	// make sure they have permission to be on the site
-
-	return "John";
-}
-
-
-
-app.use(express.static("public"));
-
 app.get("/", handleRoot);
 app.get("/home", handleHome);
-
-app.get("/home-service", function(request, response) {
-    const username = "sburton";
-    const email = "burtons@byui.edu";
-    const params = {user: username, email: email};
-    response.json(params);
-});
-
-
+app.get("/mail", handleMail);
 
 function handleRoot(request, response) {
     console.log("Receive a request for the root...");
@@ -62,6 +24,7 @@ function handleRoot(request, response) {
 }
 
 function handleHome(request, response) {
+    var name = getCurrentLoggedInUserAccount();
     console.log("Received a request for home");
     // find the username
     const username = "sburton";
@@ -69,3 +32,56 @@ function handleHome(request, response) {
     const params = {user: username, email: email};
     response.render("homepage", params);
 }
+
+function handleMail(request, response) {
+    console.log("Received a request for mail");
+
+
+    type = request.query.mail_type;
+    weight = request.query.mail_weight;
+    var rate = calculateRate(type, weight);
+
+    response.render("mail", rate);
+}
+
+function calculateRate(type, weight){
+    console.log("calculating mail rate");
+    console.log(type);
+    console.log(weight);
+
+    // calculate price
+    var price = 0;
+
+    // package
+    if (type == "package"){
+        if (weight >= 13){
+            price = "weight is too much.";
+        } else if (weight >= 12){
+            price = 5.71;
+        } else if (weight >= 9){
+            price = 5.19;
+        } else if (weight >= 5){
+            price = 4.39;
+        } else if (weight >= 0){
+            price = 3.66;
+        } else {
+            price = "weight cannot be negative.";
+        }
+    }
+
+    // large
+    else if( type == "large" || weight > 3.5){
+        price = 1 + (weight * 0.15);
+    }
+
+    // metered and stamped
+    else {
+        price = 0.50 + (weight * 0.15);
+        if (type == "stamped"){
+            price += 0.05;
+        }
+    }
+    
+    return price;
+}
+
