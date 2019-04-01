25180c8593a5700b244f6563cfe3c83452e6ba10
georgeC0stanza <32494929+georgeC0stanza@users.noreply.github.com>
2019-03-31 22:22:58 -0600

    s

--- public/cursor.js
+++ public/cursor.js
@@ -1,12 +1,13 @@
 /*!
- * Bubble Cursor.js
+ * Fairy Dust Cursor.js
  * - 90's cursors collection
  * -- https://github.com/tholman/90s-cursor-effects
- * -- http://codepen.io/tholman/full/qbxxxq/
+ * -- http://codepen.io/tholman/full/jWmZxZ/
  */
 
-(function bubblesCursor() {
+(function fairyDustCursor() {
   
+    var possibleColors = ["#D61C59", "#E7D84B", "#1B8798"]
     var width = window.innerWidth;
     var height = window.innerHeight;
     var cursor = {x: width/2, y: width/2};
@@ -20,6 +21,9 @@
     // Bind events that are needed
     function bindEvents() {
       document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('touchmove', onTouchMove);
+      document.addEventListener('touchstart', onTouchMove);
+      
       window.addEventListener('resize', onWindowResize);
     }
     
@@ -31,7 +35,7 @@
     function onTouchMove(e) {
       if( e.touches.length > 0 ) {
         for( var i = 0; i < e.touches.length; i++ ) {
-          addParticle(e.touches[i].clientX, e.touches[i].clientY);
+          addParticle( e.touches[i].clientX, e.touches[i].clientY, possibleColors[Math.floor(Math.random()*possibleColors.length)]);
         }
       }
     }
@@ -40,24 +44,24 @@
       cursor.x = e.clientX;
       cursor.y = e.clientY;
       
-      addParticle( cursor.x, cursor.y);
+      addParticle( cursor.x, cursor.y, possibleColors[Math.floor(Math.random()*possibleColors.length)]);
     }
     
-    function addParticle(x, y) {
+    function addParticle(x, y, color) {
       var particle = new Particle();
-      particle.init(x, y);
+      particle.init(x, y, color);
       particles.push(particle);
     }
     
     function updateParticles() {
       
-      // Update
+      // Updated
       for( var i = 0; i < particles.length; i++ ) {
         particles[i].update();
       }
       
       // Remove dead particles
-      for( var i = particles.length - 1; i >= 0; i-- ) {
+      for( var i = particles.length -1; i >= 0; i-- ) {
         if( particles[i].lifeSpan < 0 ) {
           particles[i].die();
           particles.splice(i, 1);
@@ -77,31 +81,31 @@
     
     function Particle() {
   
-      this.lifeSpan = 250; //ms
+      this.character = "*";
+      this.lifeSpan = 120; //ms
       this.initialStyles ={
         "position": "absolute",
         "display": "block",
         "pointerEvents": "none",
         "z-index": "10000000",
-        "width": "5px",
-        "height": "5px",
-        "background": "#e6f1f7",
-        "box-shadow": "-1px 0px #6badd3, 0px -1px #6badd3, 1px 0px #3a92c5, 0px 1px #3a92c5",
-        "border-radius": "1px",
+        "fontSize": "16px",
         "will-change": "transform"
       };
   
       // Init, and set properties
-      this.init = function(x, y) {
+      this.init = function(x, y, color) {
   
         this.velocity = {
-          x:  (Math.random() < 0.5 ? -1 : 1) * (Math.random() / 10),
-          y: (-.4 + (Math.random() * -1))
+          x:  (Math.random() < 0.5 ? -1 : 1) * (Math.random() / 2),
+          y: 1
         };
         
-        this.position = {x: x - 10, y: y - 10};
+        this.position = {x: x - 10, y: y - 20};
+        this.initialStyles.color = color;
+        console.log(color);
   
         this.element = document.createElement('span');
+        this.element.innerHTML = this.character;
         applyProperties(this.element, this.initialStyles);
         this.update();
         
@@ -111,18 +115,15 @@
       this.update = function() {
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
-        
-        // Update velocities
-        this.velocity.x += (Math.random() < 0.5 ? -1 : 1) * 2 / 75;
-        this.velocity.y -= Math.random() / 600;
         this.lifeSpan--;
         
-        this.element.style.transform = "translate3d(" + this.position.x + "px," + this.position.y + "px,0) scale(" + ( 0.2 + (250 - this.lifeSpan) / 250) + ")";
+        this.element.style.transform = "translate3d(" + this.position.x + "px," + this.position.y + "px,0) scale(" + (this.lifeSpan / 120) + ")";
       }
       
       this.die = function() {
         this.element.parentNode.removeChild(this.element);
       }
+      
     }
     
     /**
